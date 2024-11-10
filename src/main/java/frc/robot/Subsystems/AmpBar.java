e616845590fc2e85d676bfca20005ca5e9abf6eb
shmurthy7002 <yhtrum7002@gmail.com>
2024-11-10 18:27:10 -0500

    added the AmpBar

---
+++ src/main/java/frc/robot/Subsystems/AmpBar.java
@@ -0,0 +1,65 @@
+package frc.robot.Subsystems;
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AmpBar extends SubsystemBase {
+
+  private SparkMax ampBarMotor;
+  private RelativeEncoder encoder;
+  private SparkClosedLoopController controller;
+
+  public AmpBar() {
+        ampBarMotor = new SparkMax(57, MotorType.kBrushless);        
+        
+        SparkMaxConfig sparkConfig = new SparkMaxConfig();
+        sparkConfig.smartCurrentLimit(0, 15, 0);
+        ampBarMotor.configure(sparkConfig, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
+
+        encoder = ampBarMotor.getEncoder();
+        encoder.setPosition(0);
+        controller = ampBarMotor.getClosedLoopController();
+  }
+
+  public Command closedLoopControl(double setpoint){
+    return new FunctionalCommand(
+    ()-> {
+        },
+    ()-> controller.setReference(setpoint, SparkMax.ControlType.kPosition, 0),
+    interrupted-> {},
+    ()-> Math.abs(encoder.getPosition() - setpoint) < 0.1,
+    this
+);
+  }
+
+  public Command deploy(){
+    return closedLoopControl(12);
+  }
+
+  public Command retract(){
+    return closedLoopControl(0);
+  }
+
+  public boolean isDeployed(){
+    return Math.abs(encoder.getPosition()) >= 0.1;
+  }
+
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("amp bar encoder", encoder.getPosition());
+  }
+}
+
