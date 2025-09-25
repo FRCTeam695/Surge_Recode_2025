9365248d6ae3472c2dc20cd079dd69c60ca22240
Eva Zheng <eva.zheng2142147@gmail.com>
2025-09-24 23:20:32 -0400

    first commit for on sparkmax

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,5 +21,7 @@ public final class Constants {
     public static final double kP = 0.1;
     public static final double kI = 0;
     public static final double kD = 0;
+    public static final double kMaxOutput = 1;
+    public static final double kMinOutput = -1;
   }
 }
